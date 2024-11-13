af422a58e5cda84b4d1d4ee88d99e62acd4357ac
Wils Iglesias <wiglesias83@gmail.com>
2024-11-13 22:43:17 +0100

    Test authentication

--- src/Controller/ApiLoginController.php
+++ src/Controller/ApiLoginController.php
@@ -2,16 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 class ApiLoginController extends AbstractController
 {
     #[Route('/api/login', name: 'api_auth_login', methods: ['POST'])]
-    public function index(): JsonResponse
+    public function index(#[CurrentUser] ?User $user): JsonResponse
     {
-        $user = $this->getUser();
+        if (null === $user) {
+            return $this->json([
+                'message' => 'missing credentials',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
         return $this->json([
             'id' => $user->getUserIdentifier(),
         ]);
