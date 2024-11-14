34d567217c628b8599e313423ca7ca325e128bf1
Wils Iglesias <wiglesias83@gmail.com>
2024-11-14 15:26:14 +0100

    Create the request for the list of all users

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,40 +7,63 @@ use App\Form\UserType;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\SerializerInterface;
 
-#[Route('/user')]
+#[Route('/api/user')]
 final class UserController extends AbstractController
 {
-    #[Route(name: 'app_user_index', methods: ['GET'])]
-    public function index(UserRepository $userRepository): Response
+    private SerializerInterface $serializer;
+
+    public function __construct(EntityManagerInterface $entityManager, SerializerInterface $serializer)
     {
-        return $this->render('user/index.html.twig', [
-            'users' => $userRepository->findAll(),
-        ]);
+        $this->serializer = $serializer;
     }
 
-    #[Route('/new', name: 'app_user_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    #[Route('/', name: 'app_user_index', methods: ['GET'])]
+    public function index(UserRepository $userRepository): JsonResponse
     {
-        $user = new User();
-        $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
+        /** @var User[] $users */
+        $users = $userRepository->findAll();
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($user);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+        if (!$users) {
+            return $this->json([], Response::HTTP_NOT_FOUND);
         }
 
-        return $this->render('user/new.html.twig', [
-            'user' => $user,
-            'form' => $form,
-        ]);
+        $data = $this->serializer->serialize($users, 'json', ['groups' => 'user:read']);
+
+        return new JsonResponse(
+            $data,
+            Response::HTTP_OK,
+            [],
+            true
+        );
     }
+
+//    #[Route('/new', name: 'app_user_new', methods: ['GET', 'POST'])]
+//    public function new(Request $request, EntityManagerInterface $entityManager): Response
+//    {
+//        $user = new User();
+//        $form = $this->createForm(UserType::class, $user);
+//        $form->handleRequest($request);
+//
+//        if ($form->isSubmitted() && $form->isValid()) {
+//            $entityManager->persist($user);
+//            $entityManager->flush();
+//
+//            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+//        }
+//
+//        return $this->render('user/new.html.twig', [
+//            'user' => $user,
+//            'form' => $form,
+//        ]);
+//    }
 
 //    #[Route('/{id}', name: 'app_user_show', methods: ['GET'])]
 //    public function show(User $user): Response
