2db400c35f520368a16b3a84d1a97b47b42e72ca
Wils Iglesias <wiglesias83@gmail.com>
2024-11-15 14:16:38 +0100

    Configure tailwindcss and install package

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
 ];
